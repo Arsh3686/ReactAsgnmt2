ae7e7ed08ef3cd586158824c8853a2c77a81f1db
Arsh3686 <70792511+Arsh3686@users.noreply.github.com>
2023-02-16 23:08:42 +0530

    added

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import "./App.css";
+function App() {
+	const [state, setState] = useState("a");
+	console.log("bb");
+	let a = "5";
+	function ch() {
+		setState("b");
+	}
+	return (
+		<>
+			<h1 onClick={ch}>About us {state}</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse,
+				dignissimos?
+			</p>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
+				odit nesciunt fugit ad, obcaecati fuga corporis sint possimus
+				facere non vel eligendi dolores. Quas similique reiciendis quia
+				incidunt eos ea unde necessitatibus, porro maiores dolor autem,
+				dolores sequi esse molestiae sint pariatur rerum minima, ipsum
+				perferendis aliquid. Earum architecto non sequi, praesentium
+				ratione eius unde velit, odio, dicta deserunt sapiente? Ad
+				facilis veniam perferendis dicta minima, dolorem enim atque sed
+				non culpa rem voluptates corporis iusto qui dolore provident
+				ratione consectetur hic fugit nam sapiente ducimus. Illo earum
+				excepturi cumque necessitatibus labore at, voluptatibus
+				cupiditate perspiciatis ipsa aliquid velit nisi_______
+			</p>
+		</>
+	);
+}
+
+export default App;
